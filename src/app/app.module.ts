a653e823f5b9e21b9ef13d0bbc113a4f23dd3afe
Rafael Bregion <rafaelblourenco@hotmail.com>
2023-05-25 16:54:37 -0300

    deleting some photos, scripts and components, modifying cadastro-cuidador with new features and creating cadastro-idoso screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,13 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-
 import { AppComponent } from './app.component'
 import { AppRoutingModule } from './app-routing.module'
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { CadastroCuidadorComponent } from './cadastro-cuidador/cadastro-cuidador.component'
-import { CadastroIdosoComponent } from './cadastro-idoso/cadastro-idoso.component'
 import { HomeComponent } from './home/home.component'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CadastroCuidadorComponent,
-    HomeComponent,
-    CadastroIdosoComponent,
-  ],
+  declarations: [AppComponent, CadastroCuidadorComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/cadastro-cuidador/cadastro-cuidador.component.ts
+++ src/app/cadastro-cuidador/cadastro-cuidador.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-cadastro-cuidador',
   templateUrl: './cadastro-cuidador.component.html',
   styleUrls: ['./cadastro-cuidador.component.css'],
 })
-export class CadastroCuidadorComponent {}
+export class CadastroCuidadorComponent implements OnInit {
+  tab: string = 'cadastro-cuidador'
+
+  cuidador = 'Cadastro do Cuidador'
+  idoso = 'Cadastro do Idoso'
+
+  constructor() {}
+
+  ngOnInit() {}
+}
